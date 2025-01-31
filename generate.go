76ea8edb49dac4f4397a88e4f0a1567d61bdfe61
Talut Salako <salakotalut@gmail.com>
2025-01-31 10:20:41 +0100

    use go:generate command to handle sqlc and templ

--- generate.go
+++ generate.go
@@ -1,5 +1,6 @@
 package generate
 
 //go:generate templ generate
+//go:generate sqlc generate
 
 //go:generate tailwindcss-extra -i ./views/assets/css/input.css -o ./views/assets/css/tailwind.css --minify

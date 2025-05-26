61fc693f17d7872a960fa3827926cc9593a9c643
Talut Salako <salakotalut@gmail.com>
2025-05-26 15:59:30 +0100

    update dependencies

--- generate.go
+++ generate.go
@@ -1,6 +1,6 @@
 package generate
 
-//go:generate templ generate
+//go:generate go tool templ generate
 //go:generate sqlc generate
 
 //go:generate tailwindcss-extra -i ./views/assets/css/input.css -o ./views/assets/css/tailwind.css --minify
